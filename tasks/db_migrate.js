7741bbbb1f59ea233d698cac2885b5e82929504d
Matthias Zaunseder <matthias.zaunseder@hotmail.de>
2013-04-28 21:02:39 +0200

    First working version
    - Unit tests are missing
    - Documentation is missing

--- tasks/db_migrate.js
+++ tasks/db_migrate.js
@@ -10,41 +10,159 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    var tmp = require('tmp'),
+        async = grunt.util.async,
+        _ = grunt.util._;
 
-  grunt.registerMultiTask('db_migrate', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    /**
+     * Executes mysqldump on a specified database
+     * @param  {String}   filename  File where mysqldump is saved in
+     * @param  {Object}   conn      Mysql connection details
+     * @param  {Array}    tables    Which tables should be dumped?
+     * @param  {Array}    mysqldump Additional command line options for mysqldump
+     * @param  {String}   bin       Path to mysqldump binary
+     * @param  {Function} callback  Callback when dump finished
+     * @return {void}
+     */
+    function dump(filename, conn, tables, mysqldump, bin, callback) {
+        var args = mysqldump != null ? mysqldump : [];
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        args.push(
+            '-h' + conn['host'],
+            '-u' + conn.user,
+            '-p' + conn.password,
+            '--result-file=' + filename + '',
+            conn.database
+        );
+
+        if(_.isArray(tables)) {
+            args.push(tables.join(' '));
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        grunt.util.spawn({
+            cmd: bin,
+            args: args
+        }, function(error, result, code) {
+            if(!error) {
+                grunt.log.writeln('Dump created for "' + conn.database + '"');
+            }
+            else {
+                grunt.log.error('Could not create a dump for "' + conn.database + '", because: ' + error.toString());
+            }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+            callback(error);
+        });
+    }
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    /**
+     * Executes the SQL from the dump in a database
+     * @param  {String}   filename SQL File
+     * @param  {Object}   conn     Mysql connection details
+     * @param  {String}   bin      Path to mysql binary
+     * @param  {Function} callback Callback when SQL finished executing
+     * @return {void}
+     */
+    function execDump(filename, conn, bin, callback) {
+        var args = [],
+            proc;
+
+        args.push(
+            '-h' + conn.host,
+            '-u' + conn.user,
+            '-p' + conn.password,
+            '--database=' + conn.database
+        );
+
+        proc = grunt.util.spawn({
+            cmd: bin,
+            args: args
+        }, function(error, result, code) {
+            if(!error) {
+                grunt.log.writeln('Dump executed in "' + conn.database + '"');
+            }
+            else {
+                grunt.log.error('Could not execute the dump for "' + conn.database + '", because: ' + error.toString());
+            }
+
+            callback(error);
+        });
+
+        proc.stdin.setEncoding = 'utf-8';
+        proc.stdin.write('source ' + filename + '');
+        proc.stdin.end();
+    }
+
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+
+    grunt.registerMultiTask('db_migrate', 'Migrates one database to another.', function() {
+
+        var
+            // Callback when task finished
+            done = this.async(),
+
+            // Default options merged with options from gruntfile
+            options = this.options({
+                mysqldump_bin: 'bin/mysqldump',
+                mysql_bin: 'bin/mysql',
+                tables: null,
+                db_src: {
+                    host: 'localhost',
+                    user: 'test',
+                    password: 'test',
+                    database: 'migrate-source'
+                },
+                db_dest: {
+                    host: 'localhost',
+                    user: 'test',
+                    password: 'test',
+                    database: 'migrate-destination'
+                },
+                mysqldump: null
+            });
+
+
+        grunt.verbose.writeflags(options, 'Options');
+
+        async.parallel(
+            [
+                function(callback) {
+                    tmp.file({ mode: parseInt(777, 8), postfix: '-destinationbackup.sql' }, callback);
+                },
+                function(callback) {
+                    tmp.file({ mode: parseInt(777, 8), postfix: '-dump.sql' }, callback);
+                }
+            ],
+            function(err, results) {
+                if(err) {
+                    grunt.log.error(err);
+                    done(err);
+                    return;
+                }
+
+                async.series(
+                    [
+                        function(callback) {
+                            dump(results[0], options.db_dest, options.tables, options.mysqldump, options.mysqldump_bin, callback);
+                        },
+                        function(callback) {
+                            dump(results[1], options.db_src, options.tables, options.mysqldump, options.mysqldump_bin, callback);
+                        },
+                        function(callback) {
+                            execDump(results[1], options.db_dest, options.mysql_bin, callback);
+                        }
+                    ],
+                    function(err) {
+                        if(err) {
+                            grunt.log.error(err);
+                            done(err);
+                            return;
+                        }
+                        done();
+                    }
+                );
+            }
+        );
     });
-  });
 
 };

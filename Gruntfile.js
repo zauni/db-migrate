491f0b5c8b55b629aa1a821f7d3e0540934f36bb
Matthias Zaunseder <zaunseder@glanzer-und-partner.de>
2013-09-06 17:19:49 +0200

    First working version on a remote server via ssh

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,51 +10,75 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            },
+        },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
 
-    // Configuration to be run (and then tested).
-    db_migrate: {
-      default_options: {
-        options: {}
-      }
-    },
+        // Configuration to be run (and then tested).
+        db_migrate: {
+            default_options: {
+                options: {}
+            },
+            remote: {
+                options: {
+                    onRemote: true,
+                    remote: {
+                        mysqldump_bin: 'mysqldump',
+                        mysql_bin: 'mysql',
+                        host: '192.168.1.243',
+                        user: 'gup',
+                        pass: 'webunit'
+                    },
+                    db_src: {
+                        host: '192.168.1.243',
+                        user: 'test',
+                        pass: 'test',
+                        database: 'migrate-source'
+                    },
+                    db_dest: {
+                        host: '192.168.1.243',
+                        user: 'test',
+                        pass: 'test',
+                        database: 'migrate-destination'
+                    }
+                }
+            }
+        },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
+        }
 
-  });
+    });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'db_migrate', 'nodeunit']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'db_migrate', 'nodeunit']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/db_migrate.js
+++ tasks/db_migrate.js
@@ -19,18 +19,18 @@ module.exports = function(grunt) {
      * @param  {String}   filename  File where mysqldump is saved in
      * @param  {Object}   conn      Mysql connection details
      * @param  {Array}    tables    Which tables should be dumped?
-     * @param  {Array}    mysqldump Additional command line options for mysqldump
+     * @param  {Array}    mysqldump_cli_args Additional command line options for mysqldump
      * @param  {String}   bin       Path to mysqldump binary
      * @param  {Function} callback  Callback when dump finished
      * @return {void}
      */
-    function dump(filename, conn, tables, mysqldump, bin, callback) {
-        var args = mysqldump != null ? mysqldump : [];
+    function dump(filename, conn, tables, mysqldump_cli_args, bin, callback) {
+        var args = mysqldump_cli_args != null ? mysqldump_cli_args : [];
 
         args.push(
             '-h' + conn['host'],
             '-u' + conn.user,
-            '-p' + conn.password,
+            '-p' + conn.pass,
             '--result-file=' + filename + '',
             conn.database
         );
@@ -55,6 +55,50 @@ module.exports = function(grunt) {
     }
 
     /**
+     * Executes mysqldump on a remote database
+     * @param  {Object}   remote    SSH connection object
+     * @param  {String}   filename  File where mysqldump is saved in
+     * @param  {Object}   conn      Mysql connection details
+     * @param  {Array}    tables    Which tables should be dumped?
+     * @param  {Array}    mysqldump_cli_args Additional command line options for mysqldump
+     * @param  {String}   bin       Path to mysqldump binary
+     * @param  {Function} callback  Callback when dump finished
+     * @return {void}
+     */
+    function dumpRemote(remote, filename, conn, tables, mysqldump_cli_args, bin, callback) {
+        var args = mysqldump_cli_args != null ? mysqldump_cli_args : [];
+
+        args.push(
+            '-h' + conn.host,
+            '-u' + conn.user,
+            '-p' + conn.pass,
+            '--result-file=' + filename + '',
+            conn.database
+        );
+
+        if(_.isArray(tables)) {
+            args.push(tables.join(' '));
+        }
+
+        remote.exec(bin + ' ' + args.join(' '), function(err, stream) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            stream.on('exit', function(code, signal) {
+                if(code === 0) {
+                    grunt.log.writeln('Dump created for "' + conn.database + '"');
+                }
+                else {
+                    grunt.log.error('Could not create a dump for "' + conn.database + '"');
+                }
+
+                callback(code !== 0 ? 'Fehler! ' + signal : null);
+            });
+        });
+    }
+
+    /**
      * Executes the SQL from the dump in a database
      * @param  {String}   filename SQL File
      * @param  {Object}   conn     Mysql connection details
@@ -69,7 +113,7 @@ module.exports = function(grunt) {
         args.push(
             '-h' + conn.host,
             '-u' + conn.user,
-            '-p' + conn.password,
+            '-p' + conn.pass,
             '--database=' + conn.database
         );
 
@@ -92,8 +136,50 @@ module.exports = function(grunt) {
         proc.stdin.end();
     }
 
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
+    /**
+     * Executes the SQL from the dump in a remote database
+     * @param  {Object}   remote   SSH connection object
+     * @param  {String}   filename SQL File
+     * @param  {Object}   conn     Mysql connection details
+     * @param  {String}   bin      Path to mysql binary
+     * @param  {Function} callback Callback when SQL finished executing
+     * @return {void}
+     */
+    function execDumpRemote(remote, filename, conn, bin, callback) {
+        var args = [];
+
+        args.push(
+            '-h' + conn.host,
+            '-u' + conn.user,
+            '-p' + conn.pass,
+            '--database=' + conn.database
+        );
+
+        remote.exec(bin + ' ' + args.join(' '), function(error, stream) {
+            if (error) {
+                callback(error);
+                return;
+            }
+
+            stream.on('exit', function(code, signal) {
+                if(code === 0) {
+                    grunt.log.writeln('Dump executed in "' + conn.database + '"');
+                }
+                else {
+                    grunt.log.error('Could not execute the dump for "' + conn.database + '"');
+                }
+                callback(code !== 0 ? 'Fehler! ' + signal : null);
+            });
+
+            stream.setEncoding = 'utf-8';
+            stream.write('source ' + filename + '');
+            stream.end();
+        });
+    }
+
+
+
+    // Task
 
     grunt.registerMultiTask('db_migrate', 'Migrates one database to another.', function() {
 
@@ -103,53 +189,97 @@ module.exports = function(grunt) {
 
             // Default options merged with options from gruntfile
             options = this.options({
-                mysqldump_bin: 'bin/mysqldump',
-                mysql_bin: 'bin/mysql',
+                onRemote: false,
+                local: {
+                    mysqldump_bin: 'bin/mysqldump',
+                    mysql_bin: 'bin/mysql'
+                },
+                remote: {
+                    mysqldump_bin: 'mysqldump',
+                    mysql_bin: 'mysql',
+                    host: 'test.com',
+                    user: 'test',
+                    pass: 'test'
+                },
                 tables: null,
                 db_src: {
                     host: 'localhost',
                     user: 'test',
-                    password: 'test',
+                    pass: 'test',
                     database: 'migrate-source'
                 },
                 db_dest: {
                     host: 'localhost',
                     user: 'test',
-                    password: 'test',
+                    pass: 'test',
                     database: 'migrate-destination'
                 },
-                mysqldump: null
+                mysqldump_cli_args: null
             });
 
 
         grunt.verbose.writeflags(options, 'Options');
 
-        async.parallel(
-            [
-                function(callback) {
-                    tmp.file({ mode: parseInt(777, 8), postfix: '-destinationbackup.sql' }, callback);
-                },
-                function(callback) {
-                    tmp.file({ mode: parseInt(777, 8), postfix: '-dump.sql' }, callback);
-                }
-            ],
-            function(err, results) {
-                if(err) {
-                    grunt.log.error(err);
-                    done(err);
-                    return;
-                }
+        for(var key in options[options.onRemote ? 'remote' : 'local']) {
+            options[key] = options.remote[key];
+        }
 
+        if(!options.onRemote) {
+            async.parallel(
+                [
+                    function(callback) {
+                        tmp.file({ mode: parseInt(755, 8), postfix: '-destinationbackup.sql' }, callback);
+                    },
+                    function(callback) {
+                        tmp.file({ mode: parseInt(755, 8), postfix: '-dump.sql' }, callback);
+                    }
+                ],
+                function(err, results) {
+                    if(err) {
+                        grunt.log.error(err);
+                        done(err);
+                        return;
+                    }
+
+                    async.series(
+                        [
+                            function(callback) {
+                                dump(results[0], options.db_dest, options.tables, options.mysqldump_cli_args, options.mysqldump_bin, callback);
+                            },
+                            function(callback) {
+                                dump(results[1], options.db_src, options.tables, options.mysqldump_cli_args, options.mysqldump_bin, callback);
+                            },
+                            function(callback) {
+                                execDump(results[1], options.db_dest, options.mysql_bin, callback);
+                            }
+                        ],
+                        function(err) {
+                            if(err) {
+                                grunt.log.error(err);
+                                done(err);
+                                return;
+                            }
+                            done();
+                        }
+                    );
+                }
+            );
+        }
+        else {
+            var Connection = require('ssh2'),
+                c = new Connection();
+
+            c.on('ready', function() {
                 async.series(
                     [
                         function(callback) {
-                            dump(results[0], options.db_dest, options.tables, options.mysqldump, options.mysqldump_bin, callback);
+                            dumpRemote(c, '.destinationbackup.sql', options.db_dest, options.tables, options.mysqldump_cli_args, options.mysqldump_bin, callback);
                         },
                         function(callback) {
-                            dump(results[1], options.db_src, options.tables, options.mysqldump, options.mysqldump_bin, callback);
+                            dumpRemote(c, '.dump.sql', options.db_src, options.tables, options.mysqldump_cli_args, options.mysqldump_bin, callback);
                         },
                         function(callback) {
-                            execDump(results[1], options.db_dest, options.mysql_bin, callback);
+                            execDumpRemote(c, '.dump.sql', options.db_dest, options.mysql_bin, callback);
                         }
                     ],
                     function(err) {
@@ -158,11 +288,19 @@ module.exports = function(grunt) {
                             done(err);
                             return;
                         }
+                        c.end();
                         done();
                     }
                 );
-            }
-        );
+            });
+
+            c.connect({
+                host: options.host,
+                port: 22,
+                username: options.user,
+                password: options.pass
+            });
+        }
     });
 
 };
